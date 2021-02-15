4149186b02e064cf1e110157687f447b821ace3f
LucasNahuelGarcia <nahuellucasgarcia@gmail.com>
2021-02-15 01:00:17 -0300

    los componentes visuales se movieron a la carpeta io

--- lib/src/io/vistas/CrearEvento.dart
+++ lib/src/io/vistas/CrearEvento.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:scheduler/src/componentesVista/BotonFlecha.dart';
+import 'package:scheduler/src/io/componentesVista/BotonFlecha.dart';
 
 class CrearEvento extends StatelessWidget {
   @override
