cad68a1838b4a080a6ddbc2183028247328c432a
LucasNahuelGarcia <nahuellucasgarcia@gmail.com>
2021-02-20 01:38:21 -0300

    Movi la implementacion de SelectorImportanciaEvento a un archivo separado

---
+++ lib/src/io/componentesVista/SelectorImportanciaEvento.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class SelectorImportanciaEvento extends StatefulWidget {
+  final List<Color> colores;
+  final Function(int val) onChanged;
+
+  SelectorImportanciaEvento({
+    @required this.colores,
+    @required this.onChanged,
+  });
+
+  @override
+  State<StatefulWidget> createState() {
+    return StateSelectorImportanciaEvento(
+      colores: colores,
+      onChanged: onChanged,
+    );
+  }
+}
+
+class StateSelectorImportanciaEvento extends State<SelectorImportanciaEvento> {
+  final List<Color> colores;
+  final Function(int val) onChanged;
+
+  StateSelectorImportanciaEvento({
+    @required this.colores,
+    @required this.onChanged,
+  });
+
+  double _value = 0;
+  @override
+  Widget build(BuildContext context) {
+    return SliderTheme(
+      data: SliderThemeData(),
+      child: Slider(
+        label: "Importancia",
+        activeColor: colores[_value.toInt()],
+        value: _value.toDouble(),
+        divisions: colores.length - 1,
+        onChanged: (v) {
+          setState(() {
+            _value = v;
+          });
+          onChanged(v.toInt());
+        },
+        min: 0,
+        max: colores.length.toDouble() - 1,
+      ),
+    );
+  }
+}
--- lib/src/io/vistas/CrearEvento.dart
+++ lib/src/io/vistas/CrearEvento.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:scheduler/src/io/TemaEventos/TemaEventos.dart';
+import 'package:scheduler/src/io/componentesVista/SelectorImportanciaEvento.dart';
 import 'package:scheduler/src/io/vistas/FechaEvento.dart';
 import 'package:scheduler/src/storage/Evento/Evento.dart';
 import 'package:scheduler/src/storage/storage.dart';
@@ -89,53 +90,6 @@ class CrearEvento extends StatelessWidget {
           ],
         ),
       ),
-    );
-  }
-}
-
-class SelectorImportanciaEvento extends StatefulWidget {
-  final List<Color> colores;
-  final Function(int val) onChanged;
-
-  SelectorImportanciaEvento({
-    @required this.colores,
-    @required this.onChanged,
-  });
-
-  @override
-  State<StatefulWidget> createState() {
-    return StateSelectorImportanciaEvento(
-      colores: colores,
-      onChanged: onChanged,
-    );
-  }
-}
-
-class StateSelectorImportanciaEvento extends State<SelectorImportanciaEvento> {
-  final List<Color> colores;
-  final Function(int val) onChanged;
-
-  StateSelectorImportanciaEvento({
-    @required this.colores,
-    @required this.onChanged,
-  });
-
-  double _value = 0;
-  @override
-  Widget build(BuildContext context) {
-    return Slider(
-      label: "Importancia",
-      activeColor: colores[_value.toInt()],
-      value: _value.toDouble(),
-      divisions: colores.length - 1,
-      onChanged: (v) {
-        setState(() {
-          _value = v;
-        });
-        onChanged(v.toInt());
-      },
-      min: 0,
-      max: colores.length.toDouble() - 1,
     );
   }
 }
